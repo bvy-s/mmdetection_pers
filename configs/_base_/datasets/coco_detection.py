d95c8f28b5e501b9b3f9115161cd67ad5ab72a2c
Bhavya Singh <81493388+bvy-s@users.noreply.github.com>
2024-05-06 01:50:45 +0530

    Update coco_detection.py
    change path to images (for our specific dataset)

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -44,7 +44,7 @@ train_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         ann_file='annotations/instances_train2017.json',
-        data_prefix=dict(img='train2017/'),
+        data_prefix=dict(img='trainval/'),
         filter_cfg=dict(filter_empty_gt=True, min_size=32),
         pipeline=train_pipeline,
         backend_args=backend_args))
@@ -58,7 +58,7 @@ val_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         ann_file='annotations/instances_val2017.json',
-        data_prefix=dict(img='val2017/'),
+        data_prefix=dict(img='trainval/'),
         test_mode=True,
         pipeline=test_pipeline,
         backend_args=backend_args))
