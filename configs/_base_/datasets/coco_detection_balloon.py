8554bfd0cd16b1af291aceff2d74a3f24e4a1b12
bvy-s <81493388+bvy-s@users.noreply.github.com>
2024-05-08 03:12:42 +0530

    update address for balloon data

--- configs/_base_/datasets/coco_detection_balloon.py
+++ configs/_base_/datasets/coco_detection_balloon.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'data/custom/'
+data_root = 'data/coco-dataset-balloon/custom/'
 
 # Example to use different file client
 # Method 1: simply set the data root and let the file I/O module
